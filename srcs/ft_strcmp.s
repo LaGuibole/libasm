70a37c6a6b2b050e74a2a77ef304c80177dc2e26
Guillaume Philippe <157108514+LaGuibole@users.noreply.github.com>
2025-09-09 11:32:24 +0200

    ~ | code is now matching -fmacho naming convention and also compiles on linux environment

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -7,14 +7,15 @@ _loop:
     mov cl, byte[rsi + r8]
     mov dl, byte[rdi + r8]
     inc r8
+    cmp cl, dl
+    jg _exit_minus
+    jl _exit_plus
     cmp cl, 0x0
     jz _exit
     cmp dl, 0x0
     jz _exit_plus
     cmp cl, dl
     je _loop
-    jg _exit_minus
-    jl _exit_plus
 _exit:
     mov rax, 0
     ret
--- srcs/test.c
+++ srcs/test.c
@@ -2,29 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ft_strlen(char *string);
-char *ft_strcpy(char *dest, char *src);
-int ft_strcmp(char *s1, char *s2);
+int _ft_strlen(char *string);
+char *_ft_strcpy(char *dest, char *src);
+int _ft_strcmp(char *s1, char *s2);
 
 int main(void)
 {
 	char *string = "hello world!";
 	printf("og strlen = %lu\n", strlen(string));
-	printf("asm strlen = %d\n", ft_strlen(string));
+	printf("asm strlen = %d\n", _ft_strlen(string));
 	// --------------------------------------------------------------- //
 	char *og_src = "Hello You! You've succeed at strcpy apparently";
 	char *og_dest = malloc(100);
 	char *asm_dest = malloc(100);
 	char *ret_og = strcpy(og_dest, og_src);
-	char *ret_asm = ft_strcpy(asm_dest, og_src);
+	char *ret_asm = _ft_strcpy(asm_dest, og_src);
 	printf("og strcpy = %s\n", ret_og);
 	printf("asm strcpy = %s\n", ret_asm);
 	// --------------------------------------------------------------- //
-	char *s1 = "He";
-	char *s2 = "H";
+	char *s1 = "hello world";
+	char *s2 = "hello world";
 
 	printf("og strcmp = %d\n", strcmp(s1, s2));
-	printf("asm ft_strcmp = %d\n", ft_strcmp(s1, s2));
+	printf("asm ft_strcmp = %d\n", _ft_strcmp(s1, s2));
 
 	return 0;
 }
