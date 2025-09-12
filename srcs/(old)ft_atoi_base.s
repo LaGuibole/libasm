af9bdbe8c0420044a1d7f653a8e8cbca033ee9df
Guillaume Philippe <157108514+LaGuibole@users.noreply.github.com>
2025-09-12 18:13:50 +0200

    ~ | wip

---
+++ srcs/(old)ft_atoi_base.s
@@ -0,0 +1,68 @@
+bits 64
+section .data
+	FC db 9, 10, 11, 12, 13, 32 ; FC = Forbidden Chars
+section .text
+global _ft_atoi_base
+extern __errno_location
+extern _ft_strlen
+extern _ft_write
+extern _ft_putstr
+extern _ft_strchr
+extern _ft_strcmp
+_ft_atoi_base: ; int ft_atoi_base(const char *str, char *str_base)
+
+; MAPPING
+; rax = accumulateur en resultat
+; rcx = base_len
+; rdx = signe (+1 / -1)
+; r8 - r11 = temporaires
+
+_validate_base_len:
+	mov rcx, 0 ; permet de recup la len de la base
+	push rdi
+	mov	rdi, rsi ; on passe le deuxieme param de atoi base dans rdi pour le call strlen
+	call _ft_strlen
+	pop rdi
+	mov rcx, rax ; on passe la len dans rcx
+	cmp rcx, 2
+	jl _invalid_base_error ; si < 2 error
+_check_base_chars:
+    push rdi ; push rsi et rdi pour pouvoir les restaurer plus tard
+    push rsi
+	mov rdi, rsi ; inversion des params pour strchr
+_check_base_chars_loop:
+    mov sil, byte[rdi] ; le char a check
+    lea rdi, [rdi + 1] ; on stock dans rdi le reste de la chaine a checker et l'offset du pointeur
+    cmp byte[rdi], 0x0 ;
+    je _base_is_valid
+	push rdi ; rdi est ecrase par strchr, push / pop pour garder l'etat
+    call _ft_strchr
+	pop rdi
+    cmp rax, 0x0
+    je _check_base_chars_loop
+    jne _double_base_error
+_invalid_base_error:
+	mov rdi, InvalidBaseString ; on passe la string dans rdi pour pouvoir l'utiliser avec putstr
+	call _ft_putstr
+	mov rax, 0 ; on set le return a 0, error
+	ret
+_double_base_error:
+	pop rdi
+	pop rsi
+	mov rdi, DoubleBaseChar ; on passe la strind dans rdi pour putstr
+	call _ft_putstr
+	mov rax, 0 ; on set le return a 0, error
+	ret
+_base_is_valid:
+	pop rdi
+	pop rsi
+	mov rdi, BaseIsValid
+	call _ft_putstr
+	mov rax, 0
+	ret
+BaseIsValid:
+	db 'Base is Valid', 0
+InvalidBaseString:
+	db 'Invalid base', 0
+DoubleBaseChar:
+	db 'Double in base', 0
--- srcs/ft_atoi_base.s
+++ srcs/ft_atoi_base.s
@@ -1,65 +1,81 @@
 bits 64
+
 section .text
 global _ft_atoi_base
-extern __errno_location
 extern _ft_strlen
-extern _ft_write
 extern _ft_putstr
 extern _ft_strchr
-_ft_atoi_base: ; int ft_atoi_base(const char *str, char *str_base)
 
-; MAPPING
-; rax = accumulateur en resultat
-; rcx = base_len
-; rdx = signe (+1 / -1)
-; r8 - r11 = temporaires
+_ft_atoi_base:
 
-_validate_base_len:
-	mov rcx, 0 ; permet de recup la len de la base
+_validate_base:
+	; check de la len de la base
+	mov rcx, 0
 	push rdi
-	mov	rdi, rsi ; on passe le deuxieme param de atoi base dans rdi pour le call strlen
+	mov rdi, rsi
 	call _ft_strlen
 	pop rdi
-	mov rcx, rax ; on passe la len dans rcx
+	mov rcx, rax
 	cmp rcx, 2
-	jl _invalid_base_error ; si < 2 error
-_check_base_chars:
-    push rdi ; push rsi et rdi pour pouvoir les restaurer plus tard
-    push rsi
-	mov rdi, rsi ; inversion des params pour strchr
-_check_base_chars_loop:
-    mov sil, byte[rdi] ; le char a check
-    lea rdi, [rdi + 1] ; on stock dans rdi le reste de la chaine a checker et l'offset du pointeur
-    cmp byte[rdi], 0x0 ;
-    je _base_is_valid
-	push rdi ; rdi est ecrase par strchr, push / pop pour garder l'etat
-    call _ft_strchr
-	pop rdi
-    cmp rax, 0x0
-    je _check_base_chars_loop
-    jne _double_base_error
-_invalid_base_error:
-	mov rdi, InvalidBaseString ; on passe la string dans rdi pour pouvoir l'utiliser avec putstr
-	call _ft_putstr
-	mov rax, 0 ; on set le return a 0, error
-	ret
-_double_base_error:
-	pop rdi
-	pop rsi
-	mov rdi, DoubleBaseChar ; on passe la strind dans rdi pour putstr
-	call _ft_putstr
-	mov rax, 0 ; on set le return a 0, error
+	jl _invalid_base
+	; len evaluee, on check les espaces etc
+	push rdi
+	push rsi
+	mov rdi, rsi
+	_validate_base_loop:
+		movzx rsi, byte[rdi]
+		lea rdi, [rdi + 1]
+		cmp byte[rdi], 0x0
+		je _base_is_valid
+		cmp byte[rdi], 32
+		je _invalid_base
+		cmp byte[rdi], 9
+		jg _is_operator
+			_is_operator:
+				mov r8b, byte[rdi]
+				sub r8, 9
+				cmp r8, 4
+				jl _invalid_base
+		push rdi
+		call _ft_strchr
+		pop rdi
+		cmp rax, 0x0
+		je _validate_base_loop
+		jne _invalid_base
+	; les espaces et les doublons sont maintenant geres dans la base
+
+_skip_spaces:
+	_skip_space_loop:
+		cmp byte[rdi], 32
+		je _skip_space_loop
+		cmp byte[rdi], 9
+		jg _is_operator_bis
+			_is_operator_bis:
+				mov r9b, byte[rdi]
+				inc rdi
+				sub r9, 9
+				cmp r9, 4
+				jl _skip_space_loop
+	mov rax, r9
+	jmp _exit
+_exit:
 	ret
 _base_is_valid:
-	pop rdi
-	pop rsi
 	mov rdi, BaseIsValid
 	call _ft_putstr
+	pop rsi
+	pop rdi
+	jmp _skip_spaces
+_invalid_base:
+	mov rdi, InvalidBaseString
+	call _ft_putstr
+	pop rsi
+	pop rdi
 	mov rax, 0
 	ret
 BaseIsValid:
 	db 'Base is Valid', 0
 InvalidBaseString:
 	db 'Invalid base', 0
-DoubleBaseChar:
-	db 'Double in base', 0
+
+; WARNING : pour le moment, si la base est vide segfault a cause des pop rdi rsi dans _invalid_base
